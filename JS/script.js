fef898999aa2c66cdf6d9c7253a0933a3cda2321
Pinky <ezumahanthonette6@gmail.com>
2025-02-03 22:11:07 +0300

    js commentFunction

--- JS/script.js
+++ JS/script.js
@@ -8,3 +8,27 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+
+function postComment() {
+    let commentInput = 
+    document.getElementById("commentInput").ariaValueMax;
+    if (commentInput.trim() === "") {
+        alert("Write a comment before posting!");
+        return;
+    }
+    let commentList = 
+    document.getElementById("commentsList");
+    let newComment = 
+    document.createElement("p");
+      newComment.textContent = 
+      commentInput;
+      newComment.style.padding = "10px";
+      newComment.style.background = "#eee";
+      newComment.style.borderRadius = "5px";
+      newComment.style.marginTop = "5px";
+
+      commentList.appendChild(newComment);
+
+      document.getElementById("commentInput").value = ""; // Clear input
+}
